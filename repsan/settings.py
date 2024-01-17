1f2214030723b81781314804e10c4d83648d3018
Juanrch19 <juan.romero-cha@uniminuto.edu.co>
2024-01-17 09:43:57 -0500

    puebra render

--- repsan/settings.py
+++ repsan/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'repsan.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['C:\repsan\repositorio\templates'],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
