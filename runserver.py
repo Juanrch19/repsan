5b7ec54fab7f86edc0813f7eecccf4435bac8fa9
Juanrch19 <juan.romero-cha@uniminuto.edu.co>
2024-03-06 09:08:27 -0500

    ajustes plataforma

--- runserver.py
+++ runserver.py
@@ -2,7 +2,7 @@ import os
 
 def run_server():
     
-    os.system('python manage.py runserver')
+    os.system('python manage.py runserver 0.0.0.0:8000')
     
 if __name__ == "__main__":
         run_server()
